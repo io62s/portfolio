97c7a9f3b0c17f701cc311ed5c0696c116dc00b0
io62s <ufo62s@gmail.com>
2020-06-17 19:56:31 +0200

    font updt

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -12,7 +12,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: 'Raleway', sans-serif;
+    font-family: 'Source Sans Pro', sans-serif
     background-color: ${lightGrey};
   }
 `;
