20639b1dea526f65c6b2585d63c8a4086a33c2a1
io62s <ufo62s@gmail.com>
2020-07-06 01:15:15 +0200

    minor bug fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -58,7 +58,7 @@ const Home = ({ projects }) => {
             <br /> based in Belgrade, Serbia.
           </AboutMe>
           <Link to="/contact">
-            <Button primary style={btnSpring}>
+            <Button primary="true" style={btnSpring}>
               <h5 className="btn-text">contact me</h5>
             </Button>
           </Link>
